8b098b2072739c2e87f5040733a932d274bc1ebf
Eli Horne <elihorne@gmail.com>
2017-10-30 15:41:09 -0400

    sync

---
+++ public/js/function.js
@@ -0,0 +1,90 @@
+function linkHandling(){
+  $(".routing").on('click', function(event){
+    event.preventDefault();
+    var targetRoute = $(this).attr('href');
+
+    //if(window.location.pathname == '/') {
+      // we are already on the requested page
+    //  console.log('already on the home page page, scroller')
+    //} else {
+      gotoRoute(targetRoute)
+    //}
+  })
+
+  $('.scroll').on('click', function(event){
+    event.preventDefault();
+    var scrollTarget = $(this).attr('href');
+
+    if(window.location.pathname == '/' || scrollTarget == '#about') {
+      // we are already on the requested page
+      console.log('already on the home page page, scroller')
+    } else {
+      // new page, do the full route change
+
+      gotoRoute('/');
+    }
+
+    var scrollTarget = scrollTarget.split('#')[1];
+    console.log('scrollTarget = ' + scrollTarget);
+    $('html, window').animate({
+      scrollTop: $('#core-' + scrollTarget).position().top
+    }, 1000)
+  })
+}
+
+
+
+function gotoRoute(targetRoute) {
+
+  console.log('goto: ' + targetRoute)
+
+  $('html, window').animate({
+    scrollTop: 0
+  }, 1000)
+
+  $('.staging').fadeOut('slow', function(){
+    $('.top-level').hide();
+    if(targetRoute == '/') {
+      $('.staging').fadeIn();
+
+      // show home
+      $('.work-logos').removeClass('dark').addClass('pull-up');
+      $('.top-level.section-home').fadeIn(500, function(){
+        $('.top-level.section-home .content').fadeIn('slow');
+        $('.work-logos').animate({
+          'opacity' : 0.85
+        }, 1000);
+      });
+
+    } else {
+      $('.work-logos').addClass('dark');
+      $('.work-logos').removeClass('pull-up');
+      var topLevel = targetRoute.split('/')[1];
+      var secondLevel = targetRoute.split('/')[2];
+      $('.top-level.work-'+secondLevel).fadeIn();
+      $('.top-level.work-'+secondLevel).find('.content').fadeIn();
+      $('.staging').fadeIn();
+      //console.log('hello?');
+
+    }
+    // $('.top-level').fadeOut(function(){
+    // });
+  });
+
+
+  history.pushState(null, null, targetRoute);
+
+}
+
+function routing() {
+  var targetRoute = window.location.pathname;
+  console.log('routing: arrived at ' + targetRoute);
+  gotoRoute(targetRoute);
+}
+
+
+function init() {
+  // start the engines
+  routing();
+  linkHandling();
+}
