bc22086697d0dca0b709e647bee8981447752d6a
Eli Horne <elihorne@gmail.com>
2021-06-23 14:02:33 -0400

    once again stashing ribbon work

--- components/Intro.vue
+++ components/Intro.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="intro py-24 mb-10 border-b border-gray-200">
-    <div class="container px-6 mx-auto">
+  <div class="intro mb-10 border-b border-gray-200">
+    <div class="container py-20 px-6 mx-auto">
       <h1 class="text-5xl md:text-6xl mb-4 leading-tight">Hi, I'm Eli.</h1>
       <p class="text-2xl mb-4 font-bold">
         I currently work at
@@ -14,6 +14,27 @@
         <nuxt-link to="/about" class="button">Learn more about me</nuxt-link>
       </div>
     </div>
+    <!-- <div class="logo-ribbon border-t border-gray-200">
+      <div
+        class="container text-center mx-auto content-center flex justify-around"
+      >
+        <div class="p-10 content-center">
+          <img
+            class="block mx-auto"
+            src="//logo.clearbit.com/stripe.com?size=60"
+          />
+        </div>
+        <div class="border-l border-gray-200 p-10 justify-center flex">
+          <img class="" src="//logo.clearbit.com/kickstarter.com?size=60" />
+        </div>
+        <div class="border-l border-gray-200 p-10">
+          <img src="//logo.clearbit.com/google.com?size=60" />
+        </div>
+        <div class="border-l border-gray-200 p-10">
+          <img src="//logo.clearbit.com/foodnetwork.com?size=60" />
+        </div>
+      </div>
+    </div> -->
   </div>
 </template>
 <style>
